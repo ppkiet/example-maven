7123ee5fcf360c3d8fa211d07ad441260de0e32e
ppkietgit <kietppps04803@fpt.edu.vn>
2017-03-28 03:58:24 +0700

    v17

--- src/main/java/com/kietpp/maven/controller/HomeController.java
+++ src/main/java/com/kietpp/maven/controller/HomeController.java
@@ -4,7 +4,11 @@
  * and open the template in the editor.
  */
 package com.kietpp.maven.controller;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 /**
  *
  * @author PC
