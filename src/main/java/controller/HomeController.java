da49f70541905a462ab9f61fceddb248bcca8d74
ppkietgit <kietppps04803@fpt.edu.vn>
2017-03-28 03:35:40 +0700

    v8

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.kietpp.maven.controller;
+package controller;
 
 
 import org.springframework.stereotype.Controller;
